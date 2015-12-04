c5af906cb6c5c6cf5bfa8c59292397bcc1ac4d2b
k2works <kakimomokuri@gmail.com>
2015-12-04 16:16:50 +0900

    データベースアクセス処理とCommandクラス

--- lib/todo/command.rb
+++ lib/todo/command.rb
@@ -5,6 +5,42 @@ module Todo
   # コマンドラインベースの処理を行うクラスです
   # @author hiroshima-arc
   class Command
-  end
 
+    def execute
+      DB.prepare
+    end
+
+    def create_task(name, content)
+      # タスク作成時のstatusはデフォルト値が使われてNOT_YETとなる
+      Task.create!(name: name, content: content).reload
+    end
+
+    def delete_task(id)
+      task = Task.find(id)
+      task.destroy
+    end
+
+    def update_task(id, attributes)
+      if status_name = attributes[:status]
+        attributes[:status] = Task::STATUS.fetch(status_name.upcase)
+      end
+
+      task = Task.find(id)
+      task.update_attributes! attributes
+
+      task.reload
+    end
+
+    def find_tasks(status_name)
+      all_tasks = Task.order('created_at DESC')
+
+      if status_name
+        status = Task::STATUS.fetch(status_name.upcase)
+        all_tasks.status_is(status)
+      else
+        all_tasks
+      end
+    end
+
+  end
 end
